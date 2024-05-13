d21498772d04318f020d9067c41f858987b31c05
m3lies <melanie.manivanh@gmail.com>
2024-05-13 05:28:26 +0200

    refactor Participant.java

--- src/main/java/ch/sku/karatescore/model/Participant.java
+++ src/main/java/ch/sku/karatescore/model/Participant.java
@@ -1,32 +1,16 @@
 package ch.sku.karatescore.model;
 
 import ch.sku.karatescore.commons.ParticipantType;
-import ch.sku.karatescore.commons.PenaltyType;
-import ch.sku.karatescore.commons.ScoreType;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import lombok.Data;
-import lombok.Getter;
-
-import java.util.EnumMap;
-import java.util.Map;
 
 
 @Data
 public class Participant {
     private ParticipantType participantType;
 
-
-
-
-
     public Participant(ParticipantType participantType) {
         this.participantType = participantType;
 
     }
-
-
 
 }
