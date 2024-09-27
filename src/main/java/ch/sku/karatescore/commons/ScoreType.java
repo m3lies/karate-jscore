edf5c977556c7e43248a21471e6d637b68976040
m3lies <melanie.manivanh@gmail.com>
2024-09-27 23:41:07 +0200

    explicit values

--- src/main/java/ch/sku/karatescore/commons/ScoreType.java
+++ src/main/java/ch/sku/karatescore/commons/ScoreType.java
@@ -1,5 +1,19 @@
 package ch.sku.karatescore.commons;
 
+import lombok.Getter;
+
+@Getter
 public enum ScoreType {
-    YUKO,WAZARI, IPPON
+    YUKO("1 Yuko "),
+    WAZARI("2 Waza-ari"),
+    IPPON("3 Ippon");
+
+    private final String stringValue;
+
+    // Constructor to associate string with each enum constant
+    ScoreType(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+
 }
